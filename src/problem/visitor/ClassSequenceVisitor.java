6cd5e5bf9afe1a8cfc8c5c2a67ec75c1f07270b7
Josh Gayso <gaysojj@rose-hulman.edu>
2016-01-14 21:49:29 -0500

    Modified visitTypeInsn and visitMethodInsn to handle sequenceMethod for the HashMap

--- src/problem/visitor/ClassSequenceVisitor.java
+++ src/problem/visitor/ClassSequenceVisitor.java
@@ -6,13 +6,17 @@ import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
+import com.sun.xml.internal.ws.org.objectweb.asm.Type;
+
 public class ClassSequenceVisitor extends ClassVisitor {
 	private HashMap<String, String> parsedCode;
 	private int callDepth;
 	private String methodName;
-	private int counter;
+	private int counter; 
+	private String className;
 	
-	public ClassSequenceVisitor(int arg0, HashMap<String, String> parsedCode, int callDepth, String methodName, int counter) {
+	public ClassSequenceVisitor(int arg0, HashMap<String, String> parsedCode,
+			int callDepth, String methodName, int counter) {
 		super(arg0);
 		this.parsedCode = parsedCode;
 		this.callDepth = callDepth;
@@ -20,7 +24,8 @@ public class ClassSequenceVisitor extends ClassVisitor {
 		this.counter = counter;
 	}
 
-	public ClassSequenceVisitor(int arg0, ClassVisitor arg1, HashMap<String, String> parsedCode, int callDepth, String methodName, int counter) {
+	public ClassSequenceVisitor(int arg0, ClassVisitor arg1, HashMap<String, String> parsedCode,
+			int callDepth, String methodName, int counter) {
 		super(arg0, arg1);
 		this.parsedCode = parsedCode;
 		this.callDepth = callDepth;
@@ -31,11 +36,19 @@ public class ClassSequenceVisitor extends ClassVisitor {
 	@Override
 	public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
 		MethodVisitor toDecorate = super.visitMethod(access, name, desc, signature, exceptions);
+
 		if (name.equals(methodName)) {
-			MethodSequenceVisitor sequenceVisitor = new MethodSequenceVisitor(Opcodes.ASM5, toDecorate, parsedCode, callDepth, counter);
+			MethodSequenceVisitor sequenceVisitor = new MethodSequenceVisitor(Opcodes.ASM5, toDecorate, parsedCode, callDepth, counter, className);
 			return sequenceVisitor;
 		}
 		return toDecorate;
+	}
+	
+	@Override
+	public void visit(int version, int access, String name, String signature,
+			String superName, String[] interfaces) {
+		super.visit(version, access, name, signature, superName, interfaces);
+		this.className = name;
 	}
 
 }
--- src/problem/visitor/MethodSequenceVisitor.java
+++ src/problem/visitor/MethodSequenceVisitor.java
@@ -1,40 +1,57 @@
 package problem.visitor;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import javax.sound.midi.Sequence;
 
 import org.objectweb.asm.MethodVisitor;
 
+import com.sun.xml.internal.ws.org.objectweb.asm.Type;
+
 public class MethodSequenceVisitor extends MethodVisitor {
 	private HashMap<String, String> parsedCode;
 	private int callDepth;
 	private int counter;
+	private String className;
 
-	public MethodSequenceVisitor(int arg0, HashMap<String, String> parsedCode, int callDepth, int counter) {
+	public MethodSequenceVisitor(int arg0, HashMap<String, String> parsedCode, int callDepth, int counter, String className) {
 		super(arg0);
 		this.parsedCode = parsedCode;
 		this.callDepth = callDepth;
 		this.counter = counter;
+		this.className = className;
 	}
 
-	public MethodSequenceVisitor(int arg0, MethodVisitor arg1, HashMap<String, String> parsedCode, int callDepth, int counter) {
+	public MethodSequenceVisitor(int arg0, MethodVisitor arg1, HashMap<String, String> parsedCode, int callDepth, int counter, String className) {
 		super(arg0, arg1);
 		this.parsedCode = parsedCode;
 		this.callDepth = callDepth;
 		this.counter = counter;
+		this.className = className;
 	}
 	
 	@Override
 	public void visitTypeInsn(int opcode, String type) {
 		super.visitTypeInsn(opcode, type);
 		this.parsedCode.put("sequenceNode" + counter++, type+ ":hidden" );
+		this.parsedCode.put("sequenceMethod" + counter++, className + ":" + type + ":new");
 	}
 	
 	@Override
 	public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
 		super.visitMethodInsn(opcode, owner, name, desc, itf);
 		this.parsedCode.put("sequenceNode" + counter++, owner + ":nonHidden");
+		
+		Type[] argTypes = Type.getArgumentTypes(desc);
+		List<String> stypes = new ArrayList<String>();
+		
+		for (Type t: argTypes) {
+			stypes.add(t.getClassName());
+		}
+		
+		this.parsedCode.put("sequenceMethod" + counter++, className + ":" + owner + ":" + name + ":" + stypes.toString());
 	}
 
 	public HashMap<String, String> getParsedCode() {
