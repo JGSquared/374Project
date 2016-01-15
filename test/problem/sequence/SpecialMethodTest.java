fd5db45c914150ca3f703296559962ef26377ad5
Josh Gayso <gaysojj@rose-hulman.edu>
2016-01-15 12:55:42 -0500

    Test Case Removal and Prep for more Test Cases

---
+++ test/problem/sequence/SpecialMethodTest.java
@@ -0,0 +1,47 @@
+package problem.sequence;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.junit.Test;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.Opcodes;
+
+import problem.api.AbstractGraphCode;
+import problem.code.GraphUsesCode;
+import problem.visitor.ClassAssociationVisitor;
+import problem.visitor.ClassDeclarationVisitor;
+import problem.visitor.ClassFieldVisitor;
+import problem.visitor.ClassMethodVisitor;
+import problem.visitor.ClassUsesVisitor;
+
+public class SpecialMethodTest {
+	
+	@Test
+	public final void specialMethodTest() throws IOException {
+		AbstractGraphCode codeGetter = new GraphUsesCode();
+		HashMap<String, String> items = new HashMap<String, String>();
+		StringBuilder parsedCode = new StringBuilder();
+		StringBuilder expected = new StringBuilder();
+
+		ClassReader reader = new ClassReader("headfirst.factory.pizzaaf.NYPizzaIngredientFactory");
+		ClassVisitor declVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, items);
+		ClassVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5, declVisitor, items);
+		ClassVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5, fieldVisitor, items);
+		ClassVisitor usesVisitor = new ClassUsesVisitor(Opcodes.ASM5, methodVisitor, items);
+		ClassAssociationVisitor associationVisitor = new ClassAssociationVisitor(Opcodes.ASM5, usesVisitor, items);
+		reader.accept(associationVisitor, ClassReader.EXPAND_FRAMES);
+		items = associationVisitor.getParsedCode();
+		
+		GraphUsesCode guc = new GraphUsesCode();
+		String generatedCode = guc.getCode(items);
+		
+		assertTrue(generatedCode.contains("FreshClams"));
+		assertTrue(!generatedCode.contains("Veggies"));
+		assertTrue(generatedCode.contains("Mushroom"));
+		
+	}
+}
