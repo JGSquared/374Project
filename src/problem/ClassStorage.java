56e62dafe59a8205ae28ee738a72082d62cc9c10
Josh Green <greenjm@rose-hulman.edu>
2016-02-19 02:51:50 -0500

    Fixed ClassStorage and added method for setting classes to label

--- src/problem/ClassStorage.java
+++ src/problem/ClassStorage.java
@@ -3,11 +3,12 @@ package problem;
 import java.util.ArrayList;
 import java.util.List;
 
+import problem.api.IArrow;
 import problem.api.IClass;
 
 public class ClassStorage {
 	private volatile static ClassStorage uniqueController;
-	private static List<IClass> classes = new ArrayList<IClass>();
+	private List<IClass> classes = new ArrayList<IClass>();
 	
 	private ClassStorage() {
 		
@@ -24,11 +25,36 @@ public class ClassStorage {
 		return uniqueController;
 	}
 	
-	public static void addIClass(IClass iClass) {
+	public void addIClass(IClass iClass) {
 		classes.add(iClass);
 	}
 	
-	public static List<IClass> getClasses() {
+	public List<IClass> getClasses() {
 		return classes;
 	}
+	
+	public void setClassesToLabel(List<String> classNames) {
+		for (int i = 0; i < classNames.size(); i++) {
+			classNames.set(i, Helpers.getName(classNames.get(i)));
+		}
+		falsifyAllFlags();
+		for (IClass c : classes) {
+			if (classNames.contains(Helpers.getName(c.getClassName()))) {
+				c.setCanLabel(true);
+				for (IArrow a : c.getArrows()) {
+					a.setCanLabel(true);
+				}
+			}
+		}
+	}
+	
+	private void falsifyAllFlags() {
+		for (IClass c : classes) {
+			c.setCanLabel(false);
+			for (IArrow a : c.getArrows()) {
+				a.setCanLabel(false);
+			}
+		}
+	}
+	
 }
